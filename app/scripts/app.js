d16095b8d7f6fff45e1a1262b7b391db1a580e63
rajesh-rns <rajesh@synerzip.com>
2015-01-23 14:11:22 +0530

    first angular todo work

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+/**
+ * Created by rajeshs on 1/22/2015.
+ */
+var todoModule = angular.module("todoApp", []);
+todoModule.controller("MainCtrl", function ($scope) {
+    $scope.todos = [];
+
+    $scope.add = function() {
+        $scope.todos.push($scope.newtodo);
+        $scope.newtodo = {};
+    }
+
+    $scope.delete = function(item) {
+        var index = $scope.todos.indexOf(item);
+        $scope.todos.splice(index, 1);
+    }
+
+});
